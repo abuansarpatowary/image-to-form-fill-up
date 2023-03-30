1f1aff44d1609483b8fb833ed298dc9902be7ae9
abuansarpatowary <abuansar040@gmail.com>
2023-03-30 16:15:02 +0600

    UI has been modified

--- js/popup.js
+++ js/popup.js
@@ -20,7 +20,24 @@ radioButtons.forEach(radioButton => {
 // set the initial value of the "ocrEngine" field
 const initialRadioButton = document.querySelector('.radio-buttons input[type="radio"]:checked');
 formData.set('ocrEngine', initialRadioButton.value);
+function displayName(name) {
+      const words = name.split(" ");
+      let firstName, lastName;
 
+      if (words.length === 3) {
+        firstName = words.slice(0, 2).join(" ");
+        lastName = words[2];
+      } else if (words.length === 2) {
+        firstName = words[0];
+        lastName = words[1];
+      } else {
+        console.error("Invalid name format");
+      }
+      console.log(words);
+      console.log(firstName); // Output: "MOHAMMAD HABIBUR"
+  console.log(lastName); // Output: "RAHMAN"
+  
+}
 
 // Function to handle the "Extract Text" button click
 function handleExtractClick() {
@@ -34,14 +51,12 @@ function handleExtractClick() {
   // Create a new form data object and add the file to it
   formData.append('image', file);
   formData.append('language', 'eng');
-  formData.append('language', 'ara');
   formData.append('isOverlayRequired', 'true');
   formData.append('detectOrientation', 'true');
 // Console all info from form data
   for (var value of formData.values()) {
     console.log(value);
   }
-
   // Make a POST request to the OCR.Space API with the form data
   fetch('https://api.ocr.space/parse/image', {
     method: 'POST',
@@ -52,6 +67,8 @@ function handleExtractClick() {
   }) // Convert the response to JSON
   .then(response => response.json())
     .then(data => {
+     
+
       console.log(data);
     // Check that the ParsedResults property exists and is an array
     if (data.ParsedResults && Array.isArray(data.ParsedResults)) {
@@ -62,6 +79,17 @@ function handleExtractClick() {
       console.log(extractedText);
       // After the text is extracted, clear the file input
       fileInput.value = '';
+       //  response data to json
+      const text = extractedText;
+      const regex = /(?<=Name:)[^a-zA-Z\s]*(?<name>[a-zA-Z\s]+)[^a-zA-Z\s]*(?=Father's Name)/;
+      const match = text.match(regex);
+      const name = match?.groups?.name; // "MST NAJMA BEGUM"
+
+      /* const regex2 = /(?<=Father's Name)[^a-zA-Z\s]*(?<name>[a-zA-Z\s]+)[^a-zA-Z\s]*(?=Mother's Name:)/;
+      const match2 = text.match(regex2);
+      const fatherName = match2?.groups?.name; // "MST NAJMA BEGUM" */
+      displayName(name);
+
     } else {
       // Handle the case where the ParsedResults property is missing or not an array
       console.error('Invalid response data: missing or invalid ParsedResults property');
